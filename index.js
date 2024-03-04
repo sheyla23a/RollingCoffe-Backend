434e0c6ee99bc9d65e8a231e4d58a0aece9fc03a
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-04 11:17:21 -0300

    configuramos los middlewares, morgan,cors y dotenv

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from 'express';
-console.log('Hola mundo :3')
+import cors from 'cors';
+import 'dotenv/config'; //permite procesar variables de entorno
+import morgan from 'morgan';
+
 
 //1- configurar un puerto
 // puedo compilar el index.js usando node -- watch index.js en forma experimental
@@ -12,6 +15,10 @@ app.listen(app.get('port'), ()=>{
 })
 
 //2- configurar los middlewares del proyecto
-
+app.use(cors()); //permite que haga conexiones remotas
+app.use(morgan('dev')) //nos da info extra en la terminal
+app.use(express.json()) //nos permite interpretar datos en formato json
+app.use(express.urlencoded({extended:true})) //ayuda a interpretar los datos del body del request 
+//falta configurar el index.html
 
 //3- configurar las rutas
