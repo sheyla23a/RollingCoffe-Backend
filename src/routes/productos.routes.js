d4d4940e09f5a3cd26d3bd1f3263d27c5e91ee14
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-13 11:35:20 -0300

    se agrego la logica para borrar productos

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { crearProducto, editarProducto, listarProductos, obtenerProducto } from "../controllers/productos.controllers.js";
+import { borrarProducto, crearProducto, editarProducto, listarProductos, obtenerProducto } from "../controllers/productos.controllers.js";
 
 const router = Router();
 
 router.route('/productos').get(listarProductos).post(crearProducto);
-router.route('/producto/:id').get(obtenerProducto).put(editarProducto)
+router.route('/producto/:id').get(obtenerProducto).put(editarProducto).delete(borrarProducto)
 
 export default router;
