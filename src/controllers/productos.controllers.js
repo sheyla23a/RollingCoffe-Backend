459db3421d1fe094f669bd52b4a886c270af518a
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-13 10:32:46 -0300

    agregamos la funcion para obtener el producto por su ID

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -11,6 +11,18 @@ export const listarProductos = async (req, res) => {
   }
 };
 
+export const obtenerProducto = async (req,res) =>{
+    try{
+    console.log(req.params.id)
+    //si encontre el productos 
+    const productoBuscado = await Producto.findById(req.params.id);
+    res.status(200).json(productoBuscado);
+    }catch(error){
+        console.log(error)
+        res.status(404).json( {mensaje: "No se encontro el producto solicitado"})
+    }
+}
+
 export const crearProducto = async (req, res) => {
   try {
     //verificar los datos del body
